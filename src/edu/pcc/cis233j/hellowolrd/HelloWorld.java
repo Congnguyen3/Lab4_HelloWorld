01d76f707ed332df33824bb5a938b867e70a0f63
Cong Nguyen <cong.nguyen3@pcc.edu>
2017-04-24 20:15:38 -0700

    add print method

--- src/edu/pcc/cis233j/hellowolrd/HelloWorld.java
+++ src/edu/pcc/cis233j/hellowolrd/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis233j.hellowolrd;
  * Created by user on 4/24/2017.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
