d5451d40a9ef94cb14fc49e64a203b626be30497
Cong Nguyen <cong.nguyen3@pcc.edu>
2017-04-24 20:17:33 -0700

    print my name

--- src/edu/pcc/cis233j/hellowolrd/HelloWorld.java
+++ src/edu/pcc/cis233j/hellowolrd/HelloWorld.java
@@ -6,5 +6,6 @@ package edu.pcc.cis233j.hellowolrd;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("My name is Cong Nguyen.");
     }
 }
